213419dd7a9692e8218417ded92a926f4bc47e91
dexploitdm <knutdmit@gmail.com>
2019-05-11 12:38:31 +0300

    add component current quiz

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './ActiveQuiz.css'
+
+const ActiveQuiz = props => (
+    <div className="ActiveQuiz">
+        <p>
+            <span>
+                <strong>
+                    1.
+                </strong>
+                Как дела?
+            </span>
+        </p>
+
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+
+    </div>
+)
+
+export default ActiveQuiz
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react'
 import './Quiz.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
+    state = {
+        // Вопросы и враыильные ответы
+        quiz: []
+    }
+
     render() {
         return (
             <div className="Quiz">
                 <h1>Quiz</h1>
+
+                <div className="QuizWrapper">
+                    <ActiveQuiz />
+                </div>
             </div>
         )
     }
