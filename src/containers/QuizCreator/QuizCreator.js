fd9def5e51aad9605442028359be9e555da99295
Дмитрий Паршаков <knutdmit@gmail.com>
2019-05-12 20:58:11 +0500

    add pages

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './QuizCreator.css'
+
+class QuizCreator extends Component {
+    render() {
+        return (
+            <div>
+                <h1>QuizeCreator</h1>
+            </div>
+        )
+    }
+}
+export default QuizCreator
