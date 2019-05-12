91a9917d3af4d5012058c6e8e8b21be3e0f08a53
Дмитрий Паршаков <knutdmit@gmail.com>
2019-05-12 21:11:18 +0500

    add links

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react'
+import {NavLink} from 'react-router-dom'
 import './Drawer.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 
 const links = [
-    1, 2, 3
+    {to: '/', label: 'Список', exact: true},
+    {to: '/auth', label: 'Авторизация', exact: false},
+    {to: '/quize-creator', label: 'Создать тест', exact: false}
 ]
 
 class Drawer extends Component {
+
 
     renderLinks() {
         return links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a>Link { link }</a>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={'active'}
+                        onClick={this.props.onClose}
+                    >
+                        {link.label}
+                    </NavLink>
                 </li>
             )
         })
