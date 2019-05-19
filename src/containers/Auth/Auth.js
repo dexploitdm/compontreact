960ec4981e32e8ff579412d0544e0453e3e933be
dexploitdm <knutdmit@yandex.ru>
2019-05-19 13:46:02 +0500

    disable buttons on is not valid

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js'
 export default class Auth extends Component {
 
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -73,8 +74,15 @@ export default class Auth extends Component {
         control.valid = this.validateControl(control.value, control.validation)
 
         formControls[controlName] = control
+
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
         this.setState({
-            formControls
+            formControls, isFormValid
         })
         
     }
@@ -107,9 +115,16 @@ export default class Auth extends Component {
                        
                         {this.renderInputs()}
 
-
-                        <Button type="success" onClick={this.loginHandler}>Войти</Button>
-                        <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+                        <Button 
+                            type="success" 
+                            onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}    
+                        >Войти</Button>
+                        <Button 
+                            type="primary" 
+                            onClick={this.registerHandler}
+                            disabled={!this.state.isFormValid} 
+                        >Зарегистрироваться</Button>
                     </form>
                 </div>
             </div>
